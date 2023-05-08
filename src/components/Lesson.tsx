f8a0cdea822405e2031bcaafe238c6d0424b9cab
Italo <italodopeey@gmail.com>
2023-05-08 00:44:33 -0300

    Formatação de data

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,6 @@
-
 import { CheckCircle, Lock } from 'phosphor-react'
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 interface LessonProps {
   title: string;
@@ -10,35 +11,82 @@ interface LessonProps {
 
 export default function Lesson (props: LessonProps) {
 
-  const isLessonAvailable = true;
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormat = format(props.availableAt, "EEE	 '•' d 'de' MMMM '•' KK'h'mm", {
+    locale: ptBR
+  })
   
   return (
-    <a href="#">
-      <span className="text-gray-300">
-        {props.availableAt.toString()}
-      </span>
-      <div className="rounded border border-gray-500 p-4 mt-2">
-        <header className="flex items-center justify-between">
-          {isLessonAvailable ? (
-            <span className="text-sm flex items-center gap-2 text-blue-500 font-medium">
-              <CheckCircle size={20}/>
-              Conteúdo liberado
-            </span>
-          ) : (
-            <span className="text-sm flex items-center gap-2 text-orange-500 font-medium">
-              <Lock size={20}/>
-              Em breve
-            </span>
-          )}
-          <span className="text-xs rounded px-[.125rem] border border-green-300 text-white font-bold">
-            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
-          </span>
-        </header>
-        
-        <strong className="text-gray-200 mt-5 block">
-          {props.title}
-        </strong>
+    <>
+      {isLessonAvailable ? (
+        <a href="#">
+        <span className="text-gray-300">
+          {availableDateFormat}
+        </span>
+        <div className="rounded border border-gray-500 p-4 mt-2">
+          <header className="flex items-center justify-between">
+            {isLessonAvailable ? (
+              <span className="text-sm flex items-center gap-2 text-blue-500 font-medium">
+                <CheckCircle size={20}/>
+                Conteúdo liberado
+              </span>
+            ) : (
+              <span className="text-sm flex items-center gap-2 text-orange-500 font-medium">
+                <Lock size={20}/>
+                Em breve
+              </span>
+            )}
+            {props.type === 'live' ? (
+              <span className="text-xs rounded px-[.125rem] border border-green-300 text-green-300 font-bold">
+                AO VIVO
+              </span>
+            ) : (
+              <span className="text-xs rounded p-[.2rem] border border-green-300 text-white font-bold">
+                AULA PRÁTICA
+              </span>
+            )}
+          </header>
+          
+          <strong className="text-gray-200 mt-5 block">
+            {props.title}
+          </strong>
+        </div>
+      </a>
+      ): (
+        <div className='cursor-not-allowed' aria-disabled={'true'}>
+        <span className="text-gray-300">
+          {availableDateFormat}
+        </span>
+        <div className="rounded border border-gray-500 p-4 mt-2">
+          <header className="flex items-center justify-between">
+            {isLessonAvailable ? (
+              <span className="text-sm flex items-center gap-2 text-blue-500 font-medium">
+                <CheckCircle size={20}/>
+                Conteúdo liberado
+              </span>
+            ) : (
+              <span className="text-sm flex items-center gap-2 text-orange-500 font-medium">
+                <Lock size={20}/>
+                Em breve
+              </span>
+            )}
+            {props.type === 'live' ? (
+              <span className="text-xs rounded px-[.125rem] border border-green-300 text-green-300 font-bold">
+                AO VIVO
+              </span>
+            ) : (
+              <span className="text-xs rounded p-[.2rem] border border-green-300 text-white font-bold">
+                AULA PRÁTICA
+              </span>
+            )}
+          </header>
+          
+          <strong className="text-gray-200 mt-5 block">
+            {props.title}
+          </strong>
+        </div>
       </div>
-    </a>
+      )}
+    </>
   )
 }
