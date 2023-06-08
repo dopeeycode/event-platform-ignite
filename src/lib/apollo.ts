492b73294c696f4e5ebe71946557c1b9eb04c3b7
Italo <italodopeey@gmail.com>
2023-06-08 10:32:52 -0300

    fix: uri graphql

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: import.meta.env.VITE_API_URL,
+  uri: 'https://api-sa-east-1.hygraph.com/v2/clhdz6at831st01t7fk4khn15/master',
   headers: {
     'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
   },
